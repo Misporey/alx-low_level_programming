199e8a3e4c4646ac030918de7c0c3de65cc90654
misporey <rechildmisporey@gmail.com>
2021-06-17 06:56:46 -0700

    print a file with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+/*
+ * File: 5-printf.c
+ * Auth: Abdul Jabbar Abubakar
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ *,followed by a new line.
+ *
+ *Return:Alays 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return(0);
+
+
+}
