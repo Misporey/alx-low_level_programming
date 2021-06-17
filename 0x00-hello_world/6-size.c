9becc4a62cb6829ca1fdf66d3b1c59193b6a5cd5
misporey <rechildmisporey@gmail.com>
2021-06-17 07:35:37 -0700

    get the size of the computer in storing data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 
 /**
  * main - Prints the size of various types on 
- * the computer it is compiled and run on..
+ *the computer it is compiled and run on..
  *
  * Return: Always 0.
  */
@@ -15,13 +15,9 @@ int main(void)
 {
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-
 	printf("Size of an int: %zu  byte(s)\n", sizeof(int));
-
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
