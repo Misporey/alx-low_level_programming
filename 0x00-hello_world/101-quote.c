972033275ffa3f1e1ff843eeef207e73838a5c80
misporey <rechildmisporey@gmail.com>
2021-06-17 08:26:15 -0700

    print an erro file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+/*
+ * file: 101-quote.c
+ * Design by: Abdul Jabbar Abubakar
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,"
+ *followed by new line, to standard erro.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+
+
+}
