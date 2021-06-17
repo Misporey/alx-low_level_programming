1b8aaae56ed3974402ea3e7d5bf9fd78e176f97f
misporey <rechildmisporey@gmail.com>
2021-06-17 05:47:24 -0700

    created a c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+/*
+ * File:4-puts.c
+ * Devopled: Abdul Jabbar Abubakar
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual 
+ * 		  puzzle, followed by a new line.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+
+}
