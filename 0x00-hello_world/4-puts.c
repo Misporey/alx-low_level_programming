12cded4d9dfdf807a9869fe61174e363119dc208
misporey <rechildmisporey@gmail.com>
2021-06-17 06:17:22 -0700

    created a c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,9 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- *			puzzle, followed by a new line.
- * 	
- * Return: 0.
+ *puzzle, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
