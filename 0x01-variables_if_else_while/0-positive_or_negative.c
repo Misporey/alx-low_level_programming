901a7e6591a6643cd031840d620c6b781b90dcf1
misporey <rechildmisporey@gmail.com>
2021-06-20 00:21:18 -0700

    Random

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,17 +19,22 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) 
+
+	if (n > 0)
 	{
-	printf("%d is positive\n", n);
+
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n);
+
+		printf("%d is zero\n", n);
+
 	}
-	else
+	else 
 	{
-	printf("%d is negative\n", n);
+
+		printf("%d is nagetive\n", n);
 	}
 
 	return (0);
