9c3ee26e4194552f11ac637fcc6e7461f3679a1e
misporey <rechildmisporey@gmail.com>
2021-06-20 16:46:57 -0700

    print single number from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print  all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		printf(number);
+
+	}
+
+	prinf('\n');
+
+	return (0);
+
+
+}
