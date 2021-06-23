2da6a0a2eefd274171330adc5f767bbd1c40680e
misporey <rechildmisporey@gmail.com>
2021-06-23 06:14:03 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the english alphabet from a-z.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
+
