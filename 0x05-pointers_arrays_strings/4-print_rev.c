900a86c779b2a72565a9a0a11a8cca3aaba42ec0
misporey <rechildmisporey@gmail.com>
2021-06-29 05:28:30 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,25 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * print_rev - prints a string in reverse
- * @s: string s
- */
+  * print_rev - Prints a string in reverse
+  * @s: The string to print
+  *
+  * Return: void
+  */
 void print_rev(char *s)
 {
-	int size = _strlen(s);
+	int c = 0;
 
-	while (size--)
-		_putchar(*(s + size));
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+
 	_putchar('\n');
 }
