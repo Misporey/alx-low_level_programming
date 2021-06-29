f420d76c51135293e9f7c78b2baca4cca920cee4
misporey <rechildmisporey@gmail.com>
2021-06-29 09:46:13 -0700

    get string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,19 @@
 #include "holberton.h"
 
 /**
- * puts2 - prints one char out of 2 of a string, followed by
- * a new line
- * @str: string str
+ * puts2 - print every other character of a string
+ * @str: the string to print
+ *
+ * Return: void
  */
 void puts2(char *str)
 {
-	int i;
-	int size = _strlen(str);
-
-	for (i = 0; i < size; i += 2)
-		_putchar(*(str + i));
-	_putchar('\n');
+	if (str)
+	{
+		do {
+			if (*str)
+				_putchar(*str++);
+		} while (*str++);
+		_putchar('\n');
+	}
 }
