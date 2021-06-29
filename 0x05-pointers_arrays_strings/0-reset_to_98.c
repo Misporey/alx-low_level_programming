d4350601c8522637cada344fc8e4596ae1282cd9
misporey <rechildmisporey@gmail.com>
2021-06-29 04:36:36 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * main - reset_to_98 - takes a pointer to an int and updates its value to 98
+ * reset_to_98 - takes a pointer to an int and updates its value to 98
  * @n: pointer n
  */
 void reset_to_98(int *n)
